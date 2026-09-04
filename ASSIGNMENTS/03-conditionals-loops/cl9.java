0e33effd5cc4851795a3d14615e3554b9bddb5d9
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 08:27:21 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl9.java
@@ -0,0 +1,16 @@
+/* 9. Perimeter Of Equilateral Triangle */
+import java.util.*;
+class cl9{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter side of triangle");
+        float s=sc.nextFloat();
+        System.out.println("perimeter of equi triangle is  is "+(3*s));
+
+    }
+}
+/*
+enter side of triangle
+5
+perimeter of equi triangle is  is 15.0
+ */
