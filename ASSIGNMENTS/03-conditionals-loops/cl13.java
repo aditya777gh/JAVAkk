4b07323d7ade37ffdee0d3c775e99040cf5b4cb6
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 08:46:00 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl13.java
@@ -0,0 +1,16 @@
+/* 13. Perimeter Of Rhombus */
+import java.util.*;
+class cl13{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter side of rhombus");
+        double a=sc.nextFloat();
+        System.out.println("perimeter of rhombus is "+(4*a));
+
+    }
+}
+/*
+enter side of rhombus
+8
+perimeter of rhombus is 32.0
+ */
