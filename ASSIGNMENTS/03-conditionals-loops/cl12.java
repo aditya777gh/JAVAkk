320b1186ef461b04708c2948dedd03c246ea0470
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 08:43:28 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl12.java
@@ -0,0 +1,16 @@
+/* 12. Perimeter Of Square */
+import java.util.*;
+class cl12{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter side of sqare");
+        double a=sc.nextFloat();
+        System.out.println("perimeter of sqare is "+(4*a));
+
+    }
+}
+/*
+enter side of sqare
+9  
+perimeter of sqare is 36.0
+ */
