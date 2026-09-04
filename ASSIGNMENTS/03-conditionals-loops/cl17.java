60967a8eb8a3ab180918e811c9c3beb1d19e4e8c
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 10:56:32 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl17.java
@@ -0,0 +1,15 @@
+/* 17. Volume Of Sphere */
+import java.util.*;
+class cl17{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter radius ");
+        float r=sc.nextFloat();
+        System.out.println("volume of shpere is "+(4/3.0)*(Math.PI)*(Math.pow(r,3)));
+    }
+}
+/*
+enter radius 
+9
+volume of shpere is 3053.6280592892786
+ */
