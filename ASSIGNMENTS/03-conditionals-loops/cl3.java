7f1de00f54a5355ce036d8fe7232947fee25cd9c
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 06:35:59 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl3.java
@@ -0,0 +1,21 @@
+/* 3. Area Of Rectangle Program */
+import java.util.*;
+class cl3{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter base of rectangle");
+        float l=sc.nextFloat();
+        System.out.println("enter height of rectangle");
+        float b=sc.nextFloat();
+        System.out.println("area of rectangle is "+(l*b));
+
+    }
+}
+
+/*
+enter base of rectangle
+5
+enter height of rectangle
+4
+area of rectangle is 20.0
+*/
