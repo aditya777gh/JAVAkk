b48d334023917b167eed8bd6c5fffe8707cca797
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 08:32:55 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl10.java
@@ -0,0 +1,20 @@
+/*Perimeter Of Parallelogram*/
+import java.util.*;
+class cl10{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter 1st side of parallelogram");
+        double a=sc.nextFloat();
+        System.out.println("enter 2nd side of parallelogram");
+        double b=sc.nextFloat();
+        System.out.println("perimeter of parallelogram is "+(2*(a+b)));
+
+    }
+}
+/*
+enter 1st side of parallelogram
+6
+enter 2nd side of parallelogram
+7
+perimeter of parallelogram is 26.0
+ */
