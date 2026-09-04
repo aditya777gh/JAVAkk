a6c070181b5d9b1c0ca084bd2afce51ce3621278
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 11:29:32 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl20.java
@@ -0,0 +1,15 @@
+/* 20. Total Surface Area Of Cube */
+import java.util.*;
+class cl20{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter one side of cube");
+        float s=sc.nextFloat();
+        System.out.println("TSA of Cube "+6*(Math.pow(s,2)));
+    }
+}
+/*
+enter one side of cube
+7
+TSA of Cube 294.0
+ */
