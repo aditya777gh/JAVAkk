debdf56d85211c4ca0cfde27a10d8299a484ebf8
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 08:39:59 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl11.java
@@ -0,0 +1,20 @@
+/* 11. Perimeter Of Rectangle */
+import java.util.*;
+class cl11{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter length of parallelogram");
+        double a=sc.nextFloat();
+        System.out.println("enter breadth of parallelogram");
+        double b=sc.nextFloat();
+        System.out.println("perimeter of rectangle is "+(2*(a+b)));
+
+    }
+}
+/*
+enter length of parallelogram
+7 
+enter breadth of parallelogram
+12 
+perimeter of rectangle is 38.0
+ */
