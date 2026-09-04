bc2ccdffbf6832ef1fb8b78ffd7ea1eece5cb1ae
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 11:30:16 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl22.java
@@ -0,0 +1,11 @@
+/* 22. (LeetCode-1281.) Subtract the Product and Sum of Digits of an Integer */
+import java.util.*;
+class cl22 {
+    public int subtractProductAndSum(int n) {
+        
+    }
+
+    public static void main(String args[]){
+        subtractProductAndSum(int n);
+    }
+}
