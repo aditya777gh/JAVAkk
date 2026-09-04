3768f5d16e114b4af75d48ef05fe420efb296916
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 08:23:05 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl8.java
@@ -0,0 +1,16 @@
+/* 8. Perimeter Of Circle */
+import java.util.*;
+class cl8{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter radius of circle ");
+        double r=sc.nextFloat();
+        System.out.println("perimeter of circle is "+2*(Math.PI)*r);
+
+    }
+} 
+/*
+enter radius of circle 
+8
+perimeter of circle is 50.26548245743669
+ */
