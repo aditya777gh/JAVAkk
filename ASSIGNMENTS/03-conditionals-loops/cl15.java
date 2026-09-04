ecedcade6810cbe80677b63894a62c9ee1627160
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 10:19:43 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl15.java
@@ -0,0 +1,19 @@
+/* 15. Volume Of Prism */
+import java.util.*;
+class cl15{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter base ");
+        float b=sc.nextFloat();
+        System.out.println("enter height");
+        float h=sc.nextFloat();
+        System.out.println("volume of prism is "+(b*h));
+    }
+}
+/*
+enter base 
+9
+enter height
+8
+volume of prism is 72.0
+ */
