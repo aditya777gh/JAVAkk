2b53ca00b74d0c6e20777ee578176969c0e149d4
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 10:55:57 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl18.java
@@ -0,0 +1,20 @@
+/* 18. Volume Of Pyramid */
+import java.util.*;
+class cl18{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter base of triangle");
+        float b=sc.nextFloat();
+        System.out.println("enter height of triangle");
+        float h=sc.nextFloat();
+        System.out.println("area of triangle is "+(1/3.0f)*b*h);
+
+    }
+}
+/*
+enter base of triangle
+4
+enter height of triangle
+7
+area of triangle is 9.333334
+ */
