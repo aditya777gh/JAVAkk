7a456b38d12a4e84ff832be34f423348555d3492
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 06:43:02 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl5.java
@@ -0,0 +1,20 @@
+/* 5. Area Of Parallelogram */
+import java.util.*;
+class cl5{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter base of parallelogram");
+        float l=sc.nextFloat();
+        System.out.println("enter height of parallelogram");
+        float b=sc.nextFloat();
+        System.out.println("area of parallelogram is "+(l*b));
+
+    }
+}
+/*
+enter base of parallelogram
+4
+enter height of parallelogram
+9 
+area of parallelogram is 36.0
+ */
