ed86eadc28c0d82f8f50733096e00458dd0a484b
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 06:39:21 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl4.java
@@ -0,0 +1,19 @@
+/* 4. Area Of Isosceles Triangle*/
+import java.util.*;
+class cl4{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter base of triangle");
+        float b=sc.nextFloat();
+        System.out.println("enter height of triangle");
+        float h=sc.nextFloat();
+        System.out.println("area of isoceles triangle is "+(1/2.0f)*b*h);
+
+    }
+}
+/*
+enter base of triangle
+6
+enter height of triangle
+8
+area of isoceles triangle is 24.0 */
