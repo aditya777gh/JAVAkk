0afa0c4aef3847a6d6612e0783e1c4d12d688e47
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 06:31:47 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl1.java
@@ -0,0 +1,17 @@
+/* 1. Area Of Circle Java Program */
+import java.util.*;
+class cl1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter radius of circle");
+        float r=sc.nextFloat();
+        System.out.println("area of circle is "+((22.0f/7)*r*r));
+
+    }
+}
+
+/*
+enter radius of circle
+4
+area of circle is 50.285713
+ */
---
+++ ASSIGNMENTS/03-conditionals-loops/cl2.java
@@ -0,0 +1,20 @@
+/* 2. Area Of Triangle */
+import java.util.*;
+class cl2{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter base of triangle");
+        float b=sc.nextFloat();
+        System.out.println("enter height of triangle");
+        float h=sc.nextFloat();
+        System.out.println("area of triangle is "+(1/2.0f)*b*h);
+
+    }
+}
+/*
+enter base of triangle
+4
+enter height of triangle
+5
+area of triangle is 10.0
+ */
