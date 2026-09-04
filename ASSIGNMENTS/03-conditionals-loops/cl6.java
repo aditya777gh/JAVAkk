0a11c26c58be8e2b58d6ea7931aacf25dc0d552e
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-04 06:46:28 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/cl6.java
@@ -0,0 +1,20 @@
+/* 6. Area Of Rhombus*/
+import java.util.*;
+class cl6{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter base of rhombus");
+        float l=sc.nextFloat();
+        System.out.println("enter height of rhombus");
+        float b=sc.nextFloat();
+        System.out.println("area of parallelogram is "+(l*b));
+
+    }
+}
+/*
+enter base of rhombus
+12  
+enter height of rhombus
+3
+area of parallelogram is 36.0
+ */
