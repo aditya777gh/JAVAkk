114e3d76ba5709f69b0a18a53aa5c96bafe80eb9
ADITYA GUPTA <adityagupta23555@gmail.com>
2026-09-23 07:28:10 +0530

    Add files via upload

---
+++ ASSIGNMENTS/03-conditionals-loops/intermidiate/cli07.java
@@ -0,0 +1,20 @@
+// 7. Power In Java
+import java.util.*;
+class cli07{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number");
+        int  n=sc.nextInt();
+        System.out.println("enter exponent");
+        int p=sc.nextInt();
+        System.out.println("answer is "+(Math.pow(n,p)));
+    }
+}
+/*
+enter number
+5
+enter exponent
+3
+answer is 125.0
+PS D:\JAVA+DSA\JAVAkk
+ */
